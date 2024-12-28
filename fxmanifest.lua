cdd3fb02ad60c971a83ab372938642f6dc3fdaf9
Tedousek <timurantonov572012@gmail.com>
2024-12-28 17:34:04 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games 'gta5'
+lua54 'yes'
+
+author 'Tedous'
+description 'Watermark by Tedous'
+
+dependency '/onesync'
+
+ui_page 'html/index.html'
+
+files {
+    'html/img/*.png',
+    'html/index.html',
+}
